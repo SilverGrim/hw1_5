0bf758766d2d40c119aeb671bfcc4df93ac09227
Brandon Lenz <Brandon Lenz>
2016-07-20 19:41:31 -0700

    Bill Splitter

---
+++ src/homework1/Problem5.java
@@ -0,0 +1,29 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class Problem5 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Bill Amount: $");
+		double amount = input.nextDouble();
+		System.out.print("Tip amount: $");
+		double tip = input.nextDouble();
+		System.out.print("Number of People: $");
+		int ppl = input.nextInt();
+		
+		double total = 0;
+		total = ((amount*.085)+(tip+amount))/ppl;
+		
+		System.out.printf("Your total comes out to: %.2f",total);
+		input.close();
+		
+		
+		
+		
+		
+	}
+
+}
